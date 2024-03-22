65b97536188df340885093242e1e64954e72c997
Azizbek G'ulomov <azizbekgulomovweb@gmail>
2024-03-22 11:07:42 +0500

    language qo'shildi

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ const messageContainer = document.getElementById('messageContainer');
 messageContainer.scrollTop = messageContainer.scrollHeight;
 
 function scrollToBottom() {
-  messageContainer.scrollTop = messageContainer.scrollHeight;
+    messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
 const profile_info = document.querySelector('.chat .chat-navbar .right img');
@@ -43,22 +43,22 @@ const user_info_close = document.querySelector('.user-infos .top img');
 let isChatExpanded = false;
 
 profile_info.addEventListener('click', function () {
-  isChatExpanded = !isChatExpanded;
+    isChatExpanded = !isChatExpanded;
 
-  if (isChatExpanded) {
-    chat.style.width = '100%';
-    user_infos.style.marginLeft = '100%';
-    user_infos.style.display = 'none';
-} else {
-    user_infos.style.marginLeft = '';
-    chat.style.width = '';
-    user_infos.style.display = '';
-}
+    if (isChatExpanded) {
+        chat.style.width = '100%';
+        user_infos.style.marginLeft = '100%';
+        user_infos.style.display = 'none';
+    } else {
+        user_infos.style.marginLeft = '';
+        chat.style.width = '';
+        user_infos.style.display = '';
+    }
 });
 
-user_info_close.addEventListener('click', function(){
+user_info_close.addEventListener('click', function () {
     isChatExpanded = !isChatExpanded;
-    
+
     user_infos.style.transform = '100%';
     chat.style.width = '100%';
     user_infos.style.display = 'none';
@@ -66,33 +66,65 @@ user_info_close.addEventListener('click', function(){
 
 const setting = document.querySelector('.settings');
 
-function settings(e){
-    if (e === "open"){
+function settings(e) {
+    if (e === "open") {
         setting.style.display = 'flex';
-    }else{
+    } else {
         setting.style.display = 'none';
     }
 }
 
 const out = document.querySelector('.logout');
 
-function logout(e){
-    if (e === "open"){
+function logout(e) {
+    if (e === "open") {
         out.style.display = 'flex';
-    }else{
+    } else {
         out.style.display = 'none';
     }
 }
 
 const editProfile = document.querySelector('.profile-edit');
-function EditProfile(e){
-    if (e === "open"){
+function EditProfile(e) {
+    if (e === "open") {
         setting.style.display = 'none';
         editProfile.style.display = 'flex';
-    }else if (e === "back"){
+    } else if (e === "back") {
         editProfile.style.display = 'none';
         setting.style.display = 'flex';
-    }else if (e === "close"){
+    } else if (e === "close") {
         editProfile.style.display = 'none';
     }
 }
+
+const language = document.querySelector('.language');
+
+function Language(e) {
+    if (e === "open") {
+        setting.style.display = 'none';
+        language.style.display = 'flex';
+    } else if (e === 'back') {
+        language.style.display = 'none';
+        setting.style.display = 'flex'
+    } else if (e === 'close') {
+        language.style.display = 'none';
+    }
+}
+
+const lis = document.querySelectorAll('.language .bottom ul li');
+
+lis.forEach(element => {
+    const img = element.querySelector('img');
+    element.addEventListener('click', () => {
+        lis.forEach(item => {
+            const itemImg = item.querySelector('img');
+            if (item === element) {
+                img.style.display = 'inline-block';
+            } else {
+                if (itemImg && itemImg.style.display !== 'none') {
+                    itemImg.style.display = 'none';
+                }
+            }
+        });
+    });
+});
